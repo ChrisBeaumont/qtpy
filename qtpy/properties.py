f2b478d1b4152c362336fd35cb92535868655124
Chris Beaumont <beaumont@hawaii.edu>
2012-11-01 21:10:33 -0400

    list widget wrapper

--- qtpy/properties.py
+++ qtpy/properties.py
@@ -1,3 +1,9 @@
+from sip import setapi
+setapi('QVariant', 2)
+setapi('QString', 2)
+
+from PyQt4.QtCore import Qt
+
 from .util import pretty_number
 
 
@@ -57,3 +63,89 @@ class FloatLineProperty(WidgetProperty):
     def setter(self, widget, value):
         widget.setText(pretty_number(value))
         widget.editingFinished.emit()
+
+
+class ItemProxy(object):
+    def __init__(self, item):
+        self.item = item
+
+    @property
+    def label(self):
+        return self.item.text()
+
+    @label.setter
+    def label(self, value):
+        self.item.setText(value)
+
+    @property
+    def data(self):
+        return self.item.data(Qt.UserRole)
+
+    @data.setter
+    def data(self, value):
+        self.item.setData(Qt.UserRole, value)
+
+    def __iter__(self):
+        return iter([self.label, self.data])
+
+
+class ListProxy(object):
+    def __init__(self, widget):
+        self.widget = widget
+
+    def __getitem__(self, index):
+        item = self.widget.item(index)
+        if item is None:
+            raise IndexError("List index out of range")
+
+        return ItemProxy(item)
+
+    def __len__(self):
+        return self.widget.count()
+
+    @property
+    def labels(self):
+        return [self.widget.item(i).text() for i in range(self.widget.count())]
+
+    @labels.setter
+    def labels(self, values):
+        ct = len(self)
+        for i, v in enumerate(values):
+            print 'seting text %i to %s' % (i, v)
+            if i >= ct:
+                self.widget.addItem(v)
+            else:
+                self[i].text = v
+            assert self.widget.item(i).text() == v
+
+        for i in range(len(values), len(self)):
+            self.widget[i].text = ''
+
+    @property
+    def data(self):
+        return [self[i].data for i in range(len(self))]
+
+    @data.setter
+    def data(self, values):
+        ct = len(self)
+        for i, v in enumerate(values):
+            if i >= ct:
+                self.widget.addItem('')
+            self[i].data = v
+
+        for i in range(len(values), len(self)):
+            self[i].data = None
+
+    def pop(self, row):
+        item = self.widget.takeItem(row)
+        if item is None:
+            raise IndexError("List index out of range")
+        return ListProxy(item)
+
+
+class ListProperty(WidgetProperty):
+    def getter(self, widget):
+        return ListProxy(widget)
+
+    def setter(self, widget, value):
+        raise AttributeError()
--- qtpy/tests/test_properties.py
+++ qtpy/tests/test_properties.py
@@ -1,6 +1,13 @@
-from PyQt4.QtGui import QApplication, QCheckBox
+from sip import setapi
+setapi('QString', 2)
+setapi('QVariant', 2)
 
-from ..properties import ButtonProperty
+import pytest
+
+from PyQt4.QtGui import QApplication, QCheckBox, QListWidget
+from PyQt4.QtCore import Qt
+
+from ..properties import ButtonProperty, ListProperty
 
 def setup_module(module):
     module.app = QApplication([''])
@@ -8,23 +15,88 @@ def setup_module(module):
 
 class TestWidget(object):
     btn = ButtonProperty('_btn')
+    lst = ListProperty('_lst')
 
     def __init__(self):
         self._btn = QCheckBox()
+        self._lst = QListWidget()
+
+
+def assert_button_synced(prop, widget):
+    assert prop == widget.isChecked()
+
+
+def assert_list_widget_synced(prop, widget, labels=None, data=None):
+    assert len(prop) == widget.count()
+    if labels is not None:
+        assert len(prop) == len(labels)
+        assert prop.labels == labels
+
+    for i in range(len(prop)):
+        label, datum = prop[i]
+        assert widget.item(i).text() == label
+        assert widget.item(i).data(Qt.UserRole) is datum
+
+        if labels is not None:
+            assert prop[i].label == labels[i]
+
+        if data is not None:
+            assert prop[i].data is data[i]
 
 
 def test_button():
     tw = TestWidget()
-    assert tw.btn == tw._btn.isChecked()
+    assert_button_synced(tw.btn, tw._btn)
 
     tw.btn = True
-    assert tw.btn == tw._btn.isChecked()
+    assert_button_synced(tw.btn, tw._btn)
 
     tw.btn = False
-    assert tw.btn == tw._btn.isChecked()
+    assert_button_synced(tw.btn, tw._btn)
 
     tw._btn.setChecked(True)
-    assert tw.btn == tw._btn.isChecked()
+    assert_button_synced(tw.btn, tw._btn)
 
     tw._btn.setChecked(False)
-    assert tw.btn == tw._btn.isChecked()
+    assert_button_synced(tw.btn, tw._btn)
+
+
+def test_list():
+    tw = TestWidget()
+
+    tw.lst.labels = ['a', 'b', 'c']
+    assert_list_widget_synced(tw.lst, tw._lst,
+                              labels=['a', 'b', 'c'],
+                              data=[None, None, None])
+
+    tw.lst.data = [1, 2, 3]
+    assert_list_widget_synced(tw.lst, tw._lst,
+                              labels=['a', 'b', 'c'],
+                              data=[1, 2, 3])
+
+    tw.lst.data = [1, 2, 3, 4]
+    assert_list_widget_synced(tw.lst, tw._lst,
+                              labels=['a', 'b', 'c', ''],
+                              data=[1, 2, 3, 4])
+
+    tw.lst[3].label = 'd'
+    assert_list_widget_synced(tw.lst, tw._lst,
+                              labels=['a', 'b', 'c', 'd'],
+                              data=[1, 2, 3, 4])
+
+    with pytest.raises(IndexError) as exc:
+        tw.lst[4].label = 'Out of Bounds'
+    assert exc.value.args[0] == 'List index out of range'
+
+    tw.lst.pop(1)
+    assert_list_widget_synced(tw.lst, tw._lst,
+                              labels=['a', 'c', 'd'],
+                              data=[1, 3, 4])
+
+    with pytest.raises(IndexError) as exc:
+        tw.lst.pop(3)
+
+    tw.lst[0].data = 100
+    assert_list_widget_synced(tw.lst, tw._lst,
+                              labels=['a', 'c', 'd'],
+                              data=[100, 3, 4])
